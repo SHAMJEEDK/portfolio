5af1c871595d95d949b968f5de5bdc7d467cfb57
SHAMJEEDK <shamjeedk8577@gmail.com>
2024-06-24 10:54:52 +0530

    hi

---
+++ js/form.js
@@ -0,0 +1,66 @@
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = 'valid';
+    return true;
+}
+function validatePhone(){
+    var phone = document.getElementById('contact-phone').value;
+    
+    if(phone.length == 0){
+        phoneError.innerHTML = 'Phone no is required';
+        return false;
+    }
+    if(phone.length !== 10){
+        phoneError.innerHTML = 'Phone no should be 10 digits';
+        return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'only digits please';
+        return false;
+    }
+    phoneError.innerHTML = 'valid';
+    return true;
+}
+
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is not required';
+        return false;
+    }
+     if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email is Invalid';
+        return false;
+     }
+     emailError.innerHTML = 'valid';
+     return true;
+}
+function validateMessage(){
+    var message = document.getElementById('contact-message').value;
+    var required = 30;
+    var left = required - message.lenght;
+
+    if(left > 0){
+        messageError.innerHTML = left + 'more charecter required';
+        return false;
+    }
+    messageError.innerHTML = 'valid';
+    return true;
+}
